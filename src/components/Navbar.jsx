8583b27b6334a7396b3f791569e0fa8e2853bda4
Igweokolo Davidson <davidsonigweokolo@gmail.com>
2023-07-19 09:51:30 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,12 @@ const Navbar = () => {
             <h1 className="text-white font-bold text-2xl z-20">Experiences</h1>
             <HiMenuAlt3 className="z-20 text-white cursor-pointer" size={25} onClick={HandleNav}/>
             <div className={nav ? "ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10" : "absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10"}>
-                <ul className="flex flex-col fixed w-full h-full items-center justify-center">
-                    <li className="font-bold text-3xl p-8">Home</li>
-                    <li className="font-bold text-3xl p-8">Destinations</li>
-                    <li className="font-bold text-3xl p-8">Reservations</li>
-                    <li className="font-bold text-3xl p-8">Amenities</li>
-                    <li className="font-bold text-3xl p-8">Rooms</li>
+                <ul className="flex flex-col fixed w-full h-full items-center justify-center ">
+                    <li className="font-bold text-3xl p-8 cursor-pointer">Home</li>
+                    <li className="font-bold text-3xl p-8 cursor-pointer">Destinations</li>
+                    <li className="font-bold text-3xl p-8 cursor-pointer">Reservations</li>
+                    <li className="font-bold text-3xl p-8 cursor-pointer">Amenities</li>
+                    <li className="font-bold text-3xl p-8 cursor-pointer">Rooms</li>
                 </ul>
             </div>
         </div>
